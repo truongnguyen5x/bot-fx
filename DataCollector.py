1d0797988b55ff829b157f7df3c154a839961dce
truongnguyen5x <truongnguyen5x@gmail.com>
2023-06-20 09:31:12 +0700

    cm

--- DataCollector.py
+++ DataCollector.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+from datetime import datetime, timedelta
 import pandas as pd
 from pymongo import MongoClient
 import logging
@@ -40,19 +41,27 @@ def main():
     if loginResponse["status"] == False:
         print("Login failed. Error code: {0}".format(loginResponse["errorCode"]))
         return
-
+    now = datetime.now()
+    end = now + timedelta(minutes=10)
     res = client.commandExecute(
         "getChartRangeRequest",
         {
             "info": {
                 "period": 5,
-                "end": 1687168548997,
+                "end": round(end.timestamp() * 1000),
                 "start": 1673888400000,
                 "symbol": "EURUSD",
             }
         },
     )
-    print(res.returnData)
+    if res["status"] == True:
+        df = pd.DataFrame(res["returnData"]["rateInfos"])
+        digits = res["returnData"]["digits"]
+        # df["timestamp"]
+        del df["ctmString"]
+        df["timestamp"] = pd.to_datetime(df["ctm"], unit="ms")
+        records = df.to_dict("records")
+        collection.insert_many(records)
 
 
 if __name__ == "__main__":
