ba48a2cbc791dcf41381d4ff0a8265ac867ea454
truongnguyen5x <truongnguyen5x@gmail.com>
2023-06-21 14:28:12 +0700

    cm

--- DataCollector.py
+++ DataCollector.py
@@ -3,57 +3,34 @@ import os
 from datetime import datetime, timedelta
 import pandas as pd
 from pymongo import MongoClient
-import logging
+
 from xAPIConnector import APIClient, loginCommand
 import argparse
 
 load_dotenv()
-# set to true on debug environment only
-DEBUG = True
-# logger properties
-logger = logging.getLogger("jsonSocket")
-FORMAT = "[%(asctime)-15s][%(funcName)s:%(lineno)d] %(message)s"
-logging.basicConfig(format=FORMAT)
 
-if DEBUG:
-    logger.setLevel(logging.DEBUG)
-else:
-    logger.setLevel(logging.CRITICAL)
 
-# Connect to your mongodb database
 mongoClient = MongoClient(os.getenv("MONGO_CONNECTION"))
 db = mongoClient["bot_fx"]
 
 
-def collect(pair, fromTime, toTime):
+def collect(pair, fromTime):
     collection = db[pair]
     configs = db["configs"]
-    # enter your login credentials here
+
     userId = os.getenv("XTB_USER_ID")
     password = os.getenv("XTB_PASSWORD")
-
-    # create & connect to RR socket
     client = APIClient()
-
-    # connect to RR socket, login
     loginResponse = client.execute(loginCommand(userId=userId, password=password))
-    logger.info(str(loginResponse))
-
-    # check if user logged in correctly
     if loginResponse["status"] == False:
         print("Login failed. Error code: {0}".format(loginResponse["errorCode"]))
         return
 
-    # now add 10 minutes
-    now = datetime.now()
-    end = now + timedelta(minutes=10)
-
     res = client.commandExecute(
-        "getChartRangeRequest",
+        "getChartLastRequest",
         {
             "info": {
                 "period": 5,
-                "end": toTime if toTime is not None else round(end.timestamp() * 1000),
                 "start": fromTime,
                 "symbol": pair.upper(),
             }
@@ -62,11 +39,11 @@ def collect(pair, fromTime, toTime):
     if res["status"] == True:
         df = pd.DataFrame(res["returnData"]["rateInfos"])
         digits = res["returnData"]["digits"]
-        configs.replace_one(
-            {"paid": pair}, {"paid": pair, "digits": digits}, upsert=True
-        )
-        # df["timestamp"]
         del df["ctmString"]
+        df["close"] = (df["open"] + df["close"]) / pow(10, digits)
+        df["high"] = (df["open"] + df["high"]) / pow(10, digits)
+        df["low"] = (df["open"] + df["low"]) / pow(10, digits)
+        df["open"] = df["open"] / pow(10, digits)
         df["timestamp"] = pd.to_datetime(df["ctm"], unit="ms")
         print(df)
         records = df.to_dict("records")
@@ -78,9 +55,8 @@ def main():
     parser = argparse.ArgumentParser(description="Collector data candle manually")
     parser.add_argument("pair", type=str, help="currency pair")
     parser.add_argument("start", type=int, help="from timestamp")
-    parser.add_argument("-e", "--end", type=int, help="to timestamp")
     args = parser.parse_args()
-    collect(args.pair, args.start, args.end)
+    collect(args.pair, args.start)
 
 
 if __name__ == "__main__":
