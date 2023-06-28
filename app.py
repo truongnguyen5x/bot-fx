ea8002f55321d2b7c1fd201d1d1153efd0f35d9f
truongnguyen5x <truongnguyen5x@gmail.com>
2023-06-28 15:37:34 +0700

    cm

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+from bson.json_util import dumps
 
 load_dotenv()
 
@@ -11,25 +12,20 @@ app = Flask(__name__)
 mongoClient = MongoClient(os.getenv("MONGO_CONNECTION"))
 db = mongoClient["bot_fx"]
 
-# Một danh sách các đối tượng dữ liệu giả
-books = [
-    {"id": 1, "title": "Book 1", "author": "Author 1"},
-    {"id": 2, "title": "Book 2", "author": "Author 2"},
-    {"id": 3, "title": "Book 3", "author": "Author 3"},
-]
-
-
-# Định nghĩa route cho API
-@app.route("/api/books", methods=["GET"])
-def get_books():
-    return jsonify(books)
-
 
 @app.route("/pairs", methods=["GET"])
 def get_pairs():
     enabled_pairs = db.configs.find({"enabled": True})
-    print(enabled_pairs)
-    return "hello"
+    res = dumps(list(enabled_pairs))
+    return res
+
+
+@app.route("/pair/<pair>", methods=["POST"])
+def update_pair(pair):
+    data = request.get_json()
+    db.configs.update_one({"pair": pair}, {"$set": data})
+
+    return "update pair success"
 
 
 if __name__ == "__main__":
