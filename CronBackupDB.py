09b43bbf761679031ac0b358234130a574389113
truongnguyen5x <truongnguyen5x@gmail.com>
2023-07-20 07:22:25 +0000

    cm

--- CronBackupDB.py
+++ CronBackupDB.py
@@ -8,6 +8,15 @@ os.system("mongodump --out ~/mongo-backup")
 os.system("zip -r ~/backup.zip ~/mongo-backup/*")
 
 
+files = {"document": open("/root/backup.zip", "rb")}
 requests.post(
-    f'https://api.telegram.org/bot{os.getenv("TELEGRAM_BOT_TOKEN")}/sendDocument?chat_id={os.getenv("TELEGRAM_USER_ID")}&document=~/backup.zip'
+    f'https://api.telegram.org/bot{os.getenv("TELEGRAM_BOT_TOKEN")}/sendDocument',
+    data={
+        "chat_id": os.getenv("TELEGRAM_USER_ID"),
+        "parse_mode": "HTML",
+        "caption": "backup DB",
+    },
+    files=files,
+    stream=True,
 )
+
