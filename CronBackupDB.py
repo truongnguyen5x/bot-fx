39f32ed46e6af122ff30833d8ae24fe0836f6c93
truongnguyen5x <truongnguyen5x@gmail.com>
2023-07-20 14:11:17 +0700

    cm

--- CronBackupDB.py
+++ CronBackupDB.py
@@ -1,4 +1,13 @@
 import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 os.system("mongodump --out ~/mongo-backup")
 os.system("zip -r ~/backup.zip ~/mongo-backup/*")
+
+
+requests.post(
+    f'https://api.telegram.org/bot{os.getenv("TELEGRAM_BOT_TOKEN")}/sendDocument?chat_id={os.getenv("TELEGRAM_USER_ID")}&document=~/backup.zip'
+)
