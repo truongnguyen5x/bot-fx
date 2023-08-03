c5bc28242da35a4a7a3f433dfc0118485de0ba36
truongnguyen5x <truongnguyen5x@gmail.com>
2023-08-03 11:34:36 +0700

    cm

--- CronCollect.py
+++ CronCollect.py
@@ -28,6 +28,7 @@ def draw_svg(pair, limit=576):
     histories = db[pair]
     rows = list(histories.find().sort("ctm", -1).limit(limit))
     df = pd.DataFrame(rows)
+    # df["idx"] = df.reset_index().index + 1
     fig, ax = plt.subplots(figsize=(6, 2))
     ax.plot(df["timestamp"], df["close"], color="#15d90d", label=pair)
     fig.patch.set_alpha(0.0)
