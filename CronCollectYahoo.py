6949306caf2c3b56a8acc3cb5e79efd3330293d1
truongnv <truongnguyen5x@gmail.com>
2023-09-05 06:49:57 +0700

    cm

--- CronCollectYahoo.py
+++ CronCollectYahoo.py
@@ -8,8 +8,40 @@ import os
 import yfinance as yf
 import argparse
 from datetime import datetime, timedelta, timezone
+import matplotlib.pyplot as plt
 
 load_dotenv()
+
+# Create a logger object and set its level to DEBUG
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(os.path.join(os.getcwd(), "logfile.txt"))
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] %(message)s"))
+logger.addHandler(file_handler)
+
+
+def draw_svg(pair, limit=576):
+    histories = db[pair]
+    rows = list(histories.find().sort("ctm", -1).limit(limit))
+    rows.reverse()
+    df = pd.DataFrame(rows)
+    df["idx"] = df.reset_index().index + 1
+    fig, ax = plt.subplots(figsize=(6, 2))
+    ax.plot(df["idx"], df["close"], color="#15d90d", label=pair)
+    fig.patch.set_alpha(0.0)
+    ax.axis("off")
+    ax.margins(0)
+    ax.autoscale(tight=True)
+    path = os.path.join(os.getcwd(), "static", f"{pair}.svg")
+    # print(path)
+    plt.savefig(
+        path,
+        format="svg",
+        bbox_inches="tight",
+        transparent=True,
+    )
+
 
 # Connect to your mongodb database
 mongoClient = MongoClient(os.getenv("MONGO_CONNECTION"), connectTimeoutMS=2000)
@@ -27,30 +59,49 @@ logger.addHandler(file_handler)
 
 
 def symbol_label(pair):
-    return f"{pair.upper()}=X"
+    return pair["pair"]
+
+
+def symbol_label2(symbol):
+    return f'{symbol.upper().split("_")[0]}=X'
 
 
 def collect(
     timeframe,
 ):
+    configs = db["configs"]
+    pairs = configs.find()
+    list_pair = list(pairs)
     # histories = db[f"{pair}_{timeframe}"]
     # last_candle = histories.find_one({}, sort=[("ctm", pymongo.DESCENDING)])
     # TODO:
     current_time_utc = datetime.now(timezone.utc)
-    start_date_utc = current_time_utc - timedelta(minutes=3 * int(timeframe))
+
+    print(current_time_utc)
 
     symbol = list(map(symbol_label, list_pair))
+    # print(str(timeframe))
+    symbol = [x for x in symbol if x.endswith(f"_{str(timeframe)}")]
+    symbol2 = list(map(symbol_label2, symbol))
+    # print(symbol)
     interval = f"{timeframe}m"
     data = yf.download(
-        symbol, start=start_date_utc, interval=interval, group_by="ticker"
+        tickers=symbol2,
+        # start=current_time_utc,
+        period="1d",
+        interval=interval,
+        group_by="ticker",
     )
 
-    for pair in list_pair:
-        df = data[symbol_label(pair)]
+    # print(data)
+
+    for _symbol in symbol2:
+        df = data[_symbol]
         # df.rename(
         #     columns={"Open": "open", "High": "high", "Low": "low", "Close": "close"},
         #     inplace=True,
         # )
+
         df.columns = ["open", "high", "low", "close", "adj close", "vol"]
         del df["adj close"]
         del df["vol"]
@@ -60,14 +111,14 @@ def collect(
         records = df.to_dict("records")
         print(df)
 
-        # if len(records) > 0:
-        #     # logging.info(
-        #     #     f"cronjob get {len(records)} {pair} candles timeframe m{timeframe}"
-        #     # )
-        #     for record in records:
-        #         histories.update_one(
-        #             {"ctm": record["ctm"]}, {"$set": record}, upsert=True
-        #         )
+    # if len(records) > 0:
+    #     # logging.info(
+    #     #     f"cronjob get {len(records)} {pair} candles timeframe m{timeframe}"
+    #     # )
+    #     for record in records:
+    #         histories.update_one(
+    #             {"ctm": record["ctm"]}, {"$set": record}, upsert=True
+    #         )
 
 
 def main():
@@ -77,7 +128,9 @@ def main():
     args = parser.parse_args()
 
     try:
-        collect(args.timeframe if args.timeframe is not None else 5)
+        tf = args.timeframe if args.timeframe is not None else 5
+        collect(timeframe=tf)
+
     except Exception as e:
         mongoClient.close()
         logger.error(e)
